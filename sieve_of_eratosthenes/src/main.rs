bb341b78e5fcac9cc68323dca9c4b8fd4e918839
Shivam Kumar <shivam.kumar1@nutanix.com>
2026-08-12 22:02:59 +0000

    Add sieve of eratosthenes template

---
+++ sieve_of_eratosthenes/src/main.rs
@@ -0,0 +1,15 @@
+// https://www.geeksforgeeks.org/dsa/sieve-of-eratosthenes/
+
+const LIMIT: usize = 50;
+
+fn sieve_of_eratosthenes() -> usize {
+    // Print each prime number in the range from 2 to limit
+    // Return number of primes found
+    todo!()
+}
+
+fn main() {
+    let num_primes = sieve_of_eratosthenes();
+
+    println!("Number of primes found: {num_primes}");
+}
