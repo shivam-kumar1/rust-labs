2c76374ef27a34abff3414344caf138023a90c5a
Shivam Kumar <shivam.kumar1@nutanix.com>
2026-08-17 20:03:16 +0000

    Complete recursive binary search

--- binary_search_recur/src/main.rs
+++ binary_search_recur/src/main.rs
@@ -1,5 +1,16 @@
 fn binary_search(arr: [i32; 10], target: i32, left: usize, right: usize) -> isize {
-    todo!()
+    let mid = (left + right) / 2;
+    if left > right {
+        return -1;
+    }
+
+    if arr[mid] == target {
+        return mid as isize; // NP
+    } else if arr[mid] < target {
+        return binary_search(arr, target, mid + 1, right);
+    }
+
+    binary_search(arr, target, left, mid - 1)
 }
 
 fn main() {
