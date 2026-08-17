0340cf047da56de946d8adb9608c4dcabe06fb65
Shivam Kumar <shivam.kumar1@nutanix.com>
2026-08-17 19:41:18 +0000

    Complete sieve of eratosthenes

--- sieve_of_eratosthenes/src/main.rs
+++ sieve_of_eratosthenes/src/main.rs
@@ -3,9 +3,19 @@
 const LIMIT: usize = 50;
 
 fn sieve_of_eratosthenes() -> usize {
-    // Print each prime number in the range from 2 to limit
-    // Return number of primes found
-    todo!()
+    let mut num_primes: usize = 0;
+    let mut is_prime: [bool; LIMIT + 1] = [true; LIMIT + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for i in 2..=LIMIT {
+        if is_prime[i] {
+            num_primes += 1;
+            for j in (2 * i..=LIMIT).step_by(i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    num_primes
 }
 
 fn main() {
